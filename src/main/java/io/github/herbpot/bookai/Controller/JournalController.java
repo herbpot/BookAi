c3bfd5bb4e9e4da5f15534f59c2354f9eeb66496
inwoo <inwooha00@naver.com>
2024-11-13 15:37:17 +0900

    project initial

---
+++ src/main/java/io/github/herbpot/bookai/Controller/JournalController.java
@@ -0,0 +1,8 @@
+package io.github.herbpot.bookai.Controller;
+
+import java.util.logging.Logger;
+
+public class JournalController {
+    private static final Logger logger = Logger.getLogger(JournalController.class.getName());
+
+}
---
+++ src/test/java/io/github/herbpot/bookai/BookAiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.herbpot.bookai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
