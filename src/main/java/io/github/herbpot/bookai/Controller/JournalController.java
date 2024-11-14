b69a496439d64dc2883e540d82ff36fdf9e13d8a
inwoo <inwooha00@naver.com>
2024-11-14 15:26:37 +0900

    COMPLETE: complete upload Journal function

--- src/main/java/io/github/herbpot/bookai/Controller/JournalController.java
+++ src/main/java/io/github/herbpot/bookai/Controller/JournalController.java
@@ -1,17 +1,32 @@
 package io.github.herbpot.bookai.Controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import io.github.herbpot.bookai.Schema.Journal;
+import io.github.herbpot.bookai.Service.JournalService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
+@   Controller
 public class JournalController {
     private static final Logger logger = Logger.getLogger(JournalController.class.getName());
+    @Autowired
+    JournalService journalService;
 
-    @RequestMapping(value = "/write", method = RequestMethod.POST)
-    public String WriteJournal() {
+    @PostMapping(value = "/write")
+    public String WriteJournal(
+            @ModelAttribute Journal journal
+            ) {
         logger.info("Writing journal");
+        logger.info(journal.toString());
+        try{
+            journalService.insertJournal(journal);
+            logger.info("insert success");
+        }catch (Exception e){
+            logger.warning(e.getMessage());
+            throw e;
+        }
 
         return "redirect:/";
     }
